af70b2f70026a1c2a0b134674558768a5d21b43a
Dedar <thededar@gmail.com>
2024-06-15 12:08:15 +0600

    refactor app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import script
+
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -7,26 +8,20 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index():
     return render_template('search.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route('/search', methods=['POST'])
+def search():
     if request.method == 'POST':
-        text = request.form['text']
-        processed_text = text.upper()
-        script.search(processed_text)
-        return render_template('pickaboo.html')
-
+        text = request.form.get('text', '').strip()
+        if text:
+            processed_text = text.upper()
+            script.search(processed_text)
+            return render_template('result.html')
+        else:
+            return render_template('search.html', error="Please enter a search term.")
 
 @app.route('/result')
 def result():
-    #product = "samsung"
-    #script.search(product)
-    return render_template('pickaboo.html')
-    
-        
-
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#TO DO:: Import Another Python File in Flask
